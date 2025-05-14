8393177fbe35837c975b92ccc0ee81ee9e6cdc02
FlowDosNit3s <diogocosta.dc3@gmail.com>
2025-05-14 18:55:25 +0100

    match

--- src/model/match/Match.java
+++ src/model/match/Match.java
@@ -1,4 +1,102 @@
 package model.match;
 
-public class Match {
+import com.ppstudios.footballmanager.api.contracts.event.IEvent;
+import com.ppstudios.footballmanager.api.contracts.match.IMatch;
+import com.ppstudios.footballmanager.api.contracts.team.IClub;
+import com.ppstudios.footballmanager.api.contracts.team.ITeam;
+
+import java.io.IOException;
+
+public class Match implements IMatch {
+
+    ITeam homeTeam;
+    ITeam awayTeam;
+    boolean played  = false;
+
+    @Override
+    public IClub getHomeClub(){
+        if(homeTeam == null){
+            throw  new IllegalStateException("Home Club is not initialized.");
+        }
+        return homeTeam.getClub();
+    }
+
+    @Override
+    public IClub getAwayClub() {
+        if(awayTeam == null){
+            throw  new IllegalStateException("Away Club is not initialized.");
+        }
+        return awayTeam.getClub();
+    }
+
+    @Override
+    public boolean isPlayed() {
+        return played;
+    }
+
+    @Override
+    public ITeam getHomeTeam() {
+        if(homeTeam == null){
+            throw  new IllegalStateException("Home Team is not initialized.");
+        }
+        return homeTeam;
+    }
+
+    @Override
+    public ITeam getAwayTeam() {
+        if(awayTeam == null){
+            throw  new IllegalStateException("Away Team is not initialized.");
+        }
+        return awayTeam;
+    }
+
+    @Override
+    public void setPlayed() {
+        played = true;
+    }
+
+    @Override
+    public int getTotalByEvent(Class aClass, IClub iClub) {
+        return 0;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public ITeam getWinner() {
+        return null;
+    }
+
+    @Override
+    public int getRound() {
+        return 0;
+    }
+
+    @Override
+    public void setTeam(ITeam iTeam) {
+
+    }
+
+    @Override
+    public void exportToJson() throws IOException {
+
+    }
+
+    @Override
+    public void addEvent(IEvent iEvent) {
+
+    }
+
+    @Override
+    public IEvent[] getEvents() {
+        return new IEvent[0];
+    }
+
+    @Override
+    public int getEventCount() {
+        return 0;
+    }
 }
