d993692c24c2c2508ff60ea6a9bc0acf9777be15
FlowDosNit3s <diogocosta.dc3@gmail.com>
2025-05-16 18:46:27 +0100

    a

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,28 +1,19 @@
 package main;
 
-import com.ppstudios.footballmanager.api.contracts.player.IPlayer;
-import com.ppstudios.footballmanager.api.contracts.player.IPlayerPosition;
-import com.ppstudios.footballmanager.api.contracts.player.PreferredFoot;
-import com.ppstudios.footballmanager.api.contracts.team.IPlayerSelector;
+import com.ppstudios.footballmanager.api.contracts.team.IClub;
+import com.ppstudios.footballmanager.api.contracts.team.ITeam;
 import data.Importer;
-import model.player.Player;
-import model.player.PlayerPosition;
+import model.match.Match;
 import model.team.Club;
+import model.team.Team;
+import model.event.GoalEvent;
+import model.player.Player;
 
 import java.io.IOException;
-import java.time.LocalDate;
+import java.util.Random;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        Importer importer = new Importer();
-
-        Club[] clubs = importer.importData();
-
-        for (Club club : clubs) {
-            System.out.println(club);
-        }
-
+    public static void main(String[] args) {
 
     }
 }
