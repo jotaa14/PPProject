ccd2c8e24a9a52f9c0c8e546c98a454269cea363
FlowDosNit3s <diogocosta.dc3@gmail.com>
2025-05-21 07:45:05 +0100

    menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,41 +1,66 @@
 package main;
 
-import com.ppstudios.footballmanager.api.contracts.match.IMatch;
-import com.ppstudios.footballmanager.api.contracts.team.IClub;
-import data.Importer;
-import model.league.Season;
-import model.team.Club;
-
 import java.util.Scanner;
 
 import static main.Menu.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int op = mainMenu(input);
 
-        switch (op) {
-            case 1:
-                System.out.println("Starting a New Game...");
+        boolean running = true;
+        while (running) {
+            int op = mainMenu(input);
 
-                break;
-            case 2:
-                System.out.println("Loading Saved Game...");
-                //LoadSeason();
-                break;
-            case 3:
-                System.out.println("Exiting the Game. Goodbye!");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Unexpected error.");
-                break;
+            switch (op) {
+                case 1:
+                    System.out.println("Starting a New Game...");
+                    // Add logic to start a new game here
+                    runSeasonMenu(input);
+                    break;
+                case 2:
+                    System.out.println("Loading Saved Game...");
+                    // Add logic to load a saved game here
+                    runSeasonMenu(input);
+                    break;
+                case 3:
+                    System.out.println("Exiting the Game. Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
         }
 
         input.close();
     }
 
+    private static void runSeasonMenu(Scanner input) {
+        boolean inSeason = true;
+        while (inSeason) {
+            int op = seasonMenu(input);
+
+            switch (op) {
+                case 1:
+                    System.out.println("Simulating match...");
+                    // Simulate match logic
+                    break;
+                case 2:
+                    System.out.println("Starting a new league...");
+                    // Start new league logic
+                    break;
+                case 3:
+                    System.out.println("Listing information...");
+                    // List information logic
+                    break;
+                case 4:
+                    System.out.println("Exiting season menu.");
+                    inSeason = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
 }
